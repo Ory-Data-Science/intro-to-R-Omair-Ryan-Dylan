ef8d00f73c9c96a565ceefdab55f33429a9f12b6
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 14:17:28 -0500

    exercise 1 - 3 ORD
    redo of assignment

---
+++ exercise 1-3 ORD.R
@@ -0,0 +1,30 @@
+> # excercise 1 
+  print(2-10)
+print(3*5)
+print(9/2)
+print(5-3*2)
+print((5-3)*2)
+print(4**2)
+print(8/2**2)
+
+#basic variables Created new repository and copied over the previous work from sql repository
+
+mass_kg <-  2.62
+mass_g <-  mass_kg * 1000
+print(mass_g)
+#next 
+mass_lb <- 3.5
+print(mass_lb)
+mass_kg <- mass_lb/2.2
+print(mass_kg)
+
+#number 3 
+#setting up
+library(tidyverse)
+setwd(~/GitHub/Intro-to-R-Omair-Ryan-Dylan)
+# ex. 3
+average_mass <-  250
+number <- 3
+biomass <-average_mass*number
+kg_biomass <- biomass/1000
+print(kg_biomass)
