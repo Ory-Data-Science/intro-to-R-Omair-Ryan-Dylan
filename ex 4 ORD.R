68330cfa1b929cccf9dbe3b6cb8bb160b09f45c4
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 14:57:15 -0500

    ex 4 part way through
    what i've done thus far

---
+++ ex 4 ORD.R
@@ -0,0 +1,15 @@
+help("abs")
+# abs(x) computes the abs value of x, sqrt(x) computes the (principal) square root of X. 
+help("round")
+#ceiling takes a single numeric argument x and returns a numeric vector containing the smallest integers not less than the corresponding elements of x.floor takes a single numeric argument x and returns a numeric vector containing the largest integers not greater than the corresponding elements of x.Error: unexpected symbol in "floor takes"
+help("tolower")
+help("toupper")
+# same thing, Translate characters in character vectors, in particular from upper to lower case or vice versa.
+abs(-15)
+print(abs(-15))
+round(4.483847, digits = 1)
+species <- "SPECIES"
+tolower(species)
+# result ="species"
+ toupper(species)
+# result = "SPECIES"
