d4ac619f757c2303a3f2bec1c6a300bfafeb979b
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 19:43:31 -0500

    q 2 and q 3

--- exercise 7.R
+++ exercise 7.R
@@ -8,9 +8,11 @@ number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27,
 print(length(number_of_birds))
 #61
 #How many birds were counted at site 42?
-
+print(number_of_birds[42])
+#83
 #How many birds were counted at the last site? Have the computer choose the last site automatically in some way, not by manually entering its position.
-
+print(last(number_of_birds))
+#11
 #What is the total number of birds counted across all of the sites?
 
 #What is the smallest number of birds counted?
