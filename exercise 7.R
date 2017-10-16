a456b9e3e3ad084bf40b9c00a609740e6e2cc059
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 19:38:39 -0500

    numver and questions

---
+++ exercise 7.R
@@ -0,0 +1,19 @@
+number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27, 
+                     36, 25, 9, 38, 21, 12, 122, 87, 36, 3, 0, 5, 55, 62, 98, 32, 
+                     900, 33, 14, 39, 56, 81, 29, 38, 1, 0, 143, 37, 98, 77, 92, 
+                     83, 34, 98, 40, 45, 51, 17, 22, 37, 48, 38, 91, 73, 54, 46,
+                     102, 273, 600, 10, 11)
+
+#How many sites are there?
+
+#How many birds were counted at site 42?
+
+#How many birds were counted at the last site? Have the computer choose the last site automatically in some way, not by manually entering its position.
+
+#What is the total number of birds counted across all of the sites?
+
+#What is the smallest number of birds counted?
+
+#What is the largest number of birds counted?
+
+#What is the average number of birds seen at a site?
