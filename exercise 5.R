22fb1daf09b51e1a0fba0468fccc8d669c629fb7
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 19:03:46 -0500

    NTDMY number 5
    dun

--- exercise 5.R
+++ exercise 5.R
@@ -1,23 +1,24 @@
 setwd("~/GitHub/intro-to-R-Omair-Ryan-Dylan"
 library(tidyverse)
-> site1_g_carbon_m2_day <- 5 %>%
-  + site2_g_carbon_m2_day <- 2.3 
+site1_g_carbon_m2_day <- 5 
+site2_g_carbon_m2_day <- 2.3 
 Error in 5 %>% site2_g_carbon_m2_day <- 2.3 : 
-  target of assignment expands to non-language object
-> site1_area_m2 <- 200
-> site2_area_m2 <- 450
-          "print(site2_npp_day
-=/"
-        > site1_g_carbon_m2_day <- 5
-        > site2_g_carbon_m2_day <- 2.3
-        > site1_npp_day <- site1_g_carbon_m2_day * site1_area_m2
-        > site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
-        > print(site1_npp_day)
-        [1] 1000
-        > print(site2_npp_day)
-        [1] 1035
-        > print(site1_npp_day+site2_npp_day)
-        [1] 2035
-        > print(abs(site1_npp_day-site2_npp_day))
-        [1] 35
-        
+target of assignment expands to non-language object
+site1_area_m2 <- 200
+site2_area_m2 <- 450
+print(site2_npp_day)
+
+site1_g_carbon_m2_day <- 5
+site2_g_carbon_m2_day <- 2.3
+site1_npp_day <- site1_g_carbon_m2_day * site1_area_m2
+site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
+print(site1_npp_day)
+#1000
+print(site2_npp_day)
+# 1035
+print(site1_npp_day+site2_npp_day)
+#2035
+print(abs(site1_npp_day-site2_npp_day))
+# 35
+print((site1_npp_day+site2_npp_day)*365)
+# 742775
